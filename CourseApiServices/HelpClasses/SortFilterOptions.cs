dd5bb2cf5d36bde0ba61702a80c3069a1f26ee8e
Artem <artemrom1337@gmail.com>
2026-03-26 02:34:19 +0200

    Garbage clear

---
+++ CourseApiServices/HelpClasses/SortFilterOptions.cs
@@ -0,0 +1,15 @@
+using System;
+using CourseApiServices.HelpClasses;
+
+namespace CourseApiServices.Interfaces.HelpClasses;
+
+public class SortFilterOptions
+{
+      public FilterOptions Filter { get; set; } = default;
+      public SortingOptions Sorting { get; set; } = default;
+
+      public string? FilterValue { get; set; } = null;
+
+      public int PageNum { get; set; }
+
+}
