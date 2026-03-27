213eb49333c63af76189eb710d690f6542297f14
Artem <artemrom1337@gmail.com>
2026-03-27 15:17:41 +0200

    SoftDeleteFilterMigration

---
+++ CourseApiDomain/Migrations/20260327131626_SoftDeleteFilterAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseApiDomain.Migrations
+{
+    /// <inheritdoc />
+    public partial class SoftDeleteFilterAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Courses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Courses");
+        }
+    }
+}
