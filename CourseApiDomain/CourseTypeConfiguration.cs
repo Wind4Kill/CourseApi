ff84bc94db33845e026cab4bca8736a6b691b28d
Artem <artemrom1337@gmail.com>
2026-04-10 19:59:12 +0300

    Addition of user function for evaluating course rating

--- CourseApiDomain/CourseTypeConfiguration.cs
+++ CourseApiDomain/CourseTypeConfiguration.cs
@@ -9,6 +9,7 @@ public class CourseTypeConfiguration : IEntityTypeConfiguration<Course>
 {
       public void Configure(EntityTypeBuilder<Course> builder)
       {
+            builder.HasIndex(p => p.CourseName).IsUnique();
             builder.OwnsOne(c => c.CourseDetails);
             builder.Navigation(c => c.CourseDetails).IsRequired();
             builder.HasQueryFilter(c => !c.IsDeleted);
